fd6906e4451820f1a16b642e7d2e53c96d9317f0
Gilles FELIX <felix@novius.com>
2012-09-17 12:01:10 +0200

    CasperJs + firsts tests

---
+++ tests/demo.js
@@ -0,0 +1,27 @@
+casper.start('http://os1.novius.fr/admin/nos/login', function() {
+    this.test.assertExists('#login form', 'Login form is found');
+    this.fill('#login form', {
+		email: 'demo@novius-os.org',
+		password : 'demo'
+	}, true);
+});
+
+casper.then(function() {
+	this.waitForSelector("#noviusos", (function() {
+		this.test.assertTitle('Novius OS', 'Novius OS admin homepage title is the one expected');
+	}), (function() {
+        this.die("Timeout reached. No novius OS ?");
+        this.exit();
+    }));
+});
+
+casper.then(function() {
+	this.waitForSelector(".ui-sortable", (function() {
+		this.test.assertExists('#apps a[href="admin/noviusos_blog/appdesk"]', 'Launcher blog exist and sortable');
+	}), (function() {
+        this.die("Timeout reached. No launcher sortable ?");
+        this.exit();
+    }));
+});
+
+casper.run();
---
+++ tests/install.js
@@ -0,0 +1,49 @@
+casper.start('http://novius-os-test.gilles.lyon.novius.fr/install.php', function step1() {
+    this.test.assertExists('form input[type=submit][value="Move on to the next step"]', 'Move on to the next step is found');
+    this.click('form input[type=submit][value="Move on to the next step"]');
+});
+
+casper.then(function step() {
+	this.test.assertTextExists('Configuring the MySQL database', 'Step 2 loaded');
+    this.fill('form', {
+		hostname: 'lnx3.lyon.novius.fr',
+		username: 'cms_base',
+		password: 'publinova',
+		database: 'nos_gilles_noviusos_test'
+	}, true);
+});
+
+casper.then(function step3() {
+	this.test.assertTextExists('Create the first administrator account', 'Step 3 loaded');
+    this.fill('form', {
+		name: 'Test',
+		firstname: 'Test',
+		email: 'test@test.org',
+		password: 'test',
+		password_confirmation: 'test'
+	}, true);
+});
+
+casper.then(function step4() {
+	this.test.assertTextExists('Setup languages', 'Step 4 loaded');
+	this.click('a[href="admin/"]');
+});
+
+casper.then(function login() {
+    this.test.assertExists('#login form', 'Login form is found');
+    this.fill('#login form', {
+		email: 'test@test.org',
+		password : 'test'
+	}, true);
+});
+
+casper.then(function appManager() {
+	this.waitForSelector("#nos-ostabs-1", (function() {
+		this.test.assertTitle('Applications manager', 'Applications manager is loaded');
+	}), (function() {
+        this.die("Timeout reached. No Applications manager ?");
+        this.exit();
+    }));
+});
+
+casper.run();
