7d778f263f7f5e7ef8bec52beb6178a5b79cf692
Gilles FELIX <felix@novius.com>
2012-12-04 09:31:29 +0100

    Update sh tools with news submodules

--- tools/nosgit.sh
+++ tools/nosgit.sh
@@ -5,17 +5,18 @@ ROOT=$(pwd)/
 declare -A SUBMODULES
 # Bundled submodules
 SUBMODULES['novius-os/core']='novius-os';
+SUBMODULES['novius-os/noviusos_appwizard']='local/applications/noviusos_appwizard';
 SUBMODULES['novius-os/noviusos_blog']='local/applications/noviusos_blog';
 SUBMODULES['novius-os/noviusos_blognews']='local/applications/noviusos_blognews';
 SUBMODULES['novius-os/noviusos_comments']='local/applications/noviusos_comments';
+SUBMODULES['novius-os/noviusos_form']='local/applications/noviusos_form';
 SUBMODULES['novius-os/noviusos_news']='local/applications/noviusos_news';
+SUBMODULES['novius-os/noviusos_slideshow']='local/applications/noviusos_slideshow';
 SUBMODULES['novius-os/noviusos_templates_basic']='local/applications/noviusos_templates_basic';
 
 # Additional repositories
 SUBMODULES['novius-os/ci']='ci';
 SUBMODULES['novius-os/noviusos_monkey']='local/applications/noviusos_monkey';
-SUBMODULES['novius-os/noviusos_slideshow']='local/applications/noviusos_slideshow';
-SUBMODULES['novius-os/noviusos_form']='local/applications/noviusos_form';
 
 echo "Executing git $* on every submodule"
 
--- tools/nosremote.sh
+++ tools/nosremote.sh
@@ -6,8 +6,6 @@ declare -A SUBMODULES
 
 SUBMODULES['novius-os/ci']='ci';
 SUBMODULES['novius-os/noviusos_monkey']='local/applications/noviusos_monkey';
-SUBMODULES['novius-os/noviusos_slideshow']='local/applications/noviusos_slideshow';
-SUBMODULES['novius-os/noviusos_form']='local/applications/noviusos_form';
 
 ##################################
 # Create additional repositories #
@@ -29,10 +27,13 @@ done
 # Add the 'github' remote to every repository #
 ###############################################
 SUBMODULES['novius-os/core']='novius-os';
+SUBMODULES['novius-os/noviusos_appwizard']='local/applications/noviusos_appwizard';
 SUBMODULES['novius-os/noviusos_blog']='local/applications/noviusos_blog';
 SUBMODULES['novius-os/noviusos_blognews']='local/applications/noviusos_blognews';
 SUBMODULES['novius-os/noviusos_comments']='local/applications/noviusos_comments';
+SUBMODULES['novius-os/noviusos_form']='local/applications/noviusos_form';
 SUBMODULES['novius-os/noviusos_news']='local/applications/noviusos_news';
+SUBMODULES['novius-os/noviusos_slideshow']='local/applications/noviusos_slideshow';
 SUBMODULES['novius-os/noviusos_templates_basic']='local/applications/noviusos_templates_basic';
 
 git remote add github git@github.com:novius-os/novius-os.git
