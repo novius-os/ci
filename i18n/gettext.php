b9472b3d92550e42bfe6494c2a65b938425104c8
esperat <esperat@novius.fr>
2012-12-05 16:52:54 +0100

    Guidelines

--- i18n/gettext.php
+++ i18n/gettext.php
@@ -3,8 +3,9 @@
 define('CWD', getcwd().'/po');
 define('LANG', $argv[1]);
 
-function __($msg) {
-	return $msg;
+function __($msg)
+{
+    return $msg;
 }
 
 if (!empty($argv[2])) {
@@ -21,13 +22,13 @@ $files = new RecursiveIteratorIterator($directory);
 $dicts = array();
 $all = array();
 foreach ($files as $file) {
-	$filename =  $file->getFilename();
-	if (substr($filename, -4) != '.php') {
-		continue;
-	}
-	$pathname = $file->getPathname();
+    $filename =  $file->getFilename();
+    if (substr($filename, -4) != '.php') {
+        continue;
+    }
+    $pathname = $file->getPathname();
     $dict_name = substr(str_replace('.lang.php', '.php', str_replace(CWD.'/lang/'.LANG.'/', '', $pathname)), 0, -4);
-	$dicts[$dict_name] = include $pathname;
+    $dicts[$dict_name] = include $pathname;
     foreach ($dicts[$dict_name] as $msgid => $msgstr) {
         if (empty($all[$msgid])) {
             $all[$msgid] = array($msgstr, $dict_name);
@@ -43,13 +44,13 @@ $found = array();
 $all = array();
 $current = '';
 foreach ($files as $file) {
-	$filename =  $file->getFilename();
-	if (substr($filename, -3) != '.po') {
-		continue;
-	}
+    $filename =  $file->getFilename();
+    if (substr($filename, -3) != '.po') {
+        continue;
+    }
 
-	$pathname = $file->getPathname();
-	$cleaned_file = str_replace(CWD.'/', '', $pathname);
+    $pathname = $file->getPathname();
+    $cleaned_file = str_replace(CWD.'/', '', $pathname);
     if (preg_match('`lang/`', $cleaned_file)) {
         continue;
     }
@@ -66,18 +67,18 @@ foreach ($files as $file) {
     }
     //$dict_files[$dict_name][$cleaned_file] = true;
     //$files_dict[$dict_name][$cleaned_file] = $dict_name;
-	$po = file($pathname, FILE_IGNORE_NEW_LINES);
+    $po = file($pathname, FILE_IGNORE_NEW_LINES);
     $po[] = "";
 
     $msgcomment = array();
     $msgusage = array();
     $msgid = array();
     $msgstr = array();
-	foreach ($po as $line) {
+    foreach ($po as $line) {
         if (empty($line)) {
-			// Skip empty msgid (headers)
+            // Skip empty msgid (headers)
             $msgid = implode("\n", $msgid);
-			if (!empty($msgid)) {
+            if (!empty($msgid)) {
                 $msg = array(
                     'str' => implode("\n", $msgstr),
                     'comment' => implode("\n", $msgcomment),
@@ -93,7 +94,7 @@ foreach ($files as $file) {
                     $all[$msgid]['usage']   .= ($usage ? "\n" : '').$msg['usage'];
                 }
                 unset($msg);
-			}
+            }
 
             $msgcomment = array();
             $msgusage = array();
@@ -114,7 +115,7 @@ foreach ($files as $file) {
                 $msgstr[] = substr(trim(substr($line, 6)), 1, -1);
             }
         }
-	}
+    }
 }
 
 $unused = array();
@@ -136,7 +137,8 @@ foreach ($dicts as $dict_name => $messages) {
 }
 
 
-function dict_stat($messages) {
+function dict_stat($messages)
+{
     $stat = array(
         'word_count' => 0,
         'word_count_translated' => 0,
