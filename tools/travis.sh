56c920243ff3fcc9499d062ae826543ca58b6bcc
Gilles FELIX <felix@novius.com>
2013-06-28 15:29:05 +0200

    New tools scripts for test and travis

--- tools/travis.sh
+++ tools/travis.sh
@@ -7,4 +7,4 @@ export PHANTOMJS_EXECUTABLE='phantomjs --local-to-remote-url-access=yes --ignore
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 
-"DISPLAY=:99.0 $TEST_SCRIPT run"
+DISPLAY=:99.0 $TEST_SCRIPT run
---
+++ tools/upload.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+IMAGESHACK_DEVELOPER_KEY=8ABCDELQ673cd7e375ad15fa94a10c45b9a699f9
+export IMAGESHACK_DEVELOPER_KEY
+
+wget http://www.novius-os.org/static/apps/noviusos_templates_basic/img/logo.png -O /tmp/logo-novius-os.png
+
+./imageshack-upload /tmp/logo-novius-os.png
