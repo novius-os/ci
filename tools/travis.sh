d44703abcf41b1331703bcf8944707573e7163d0
Gilles FELIX <felix@novius.com>
2013-06-28 15:21:29 +0200

    New tools scripts for test and travis

--- tools/travis.sh
+++ tools/travis.sh
@@ -1,4 +1,10 @@
 #! /bin/sh
 
 ROOT=$(pwd)/
-"DISPLAY=:99.0 $ROOTtest.sh run"
+TEST_SCRIPT=$ROOT'test.sh'
+
+export PHANTOMJS_EXECUTABLE='phantomjs --local-to-remote-url-access=yes --ignore-ssl-errors=yes'
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+
+"DISPLAY=:99.0 $TEST_SCRIPT run"
