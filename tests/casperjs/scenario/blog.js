05cf69f47acb72ff821bb5defa6a4666f3d4dfd0
Gilles FELIX <felix@novius.com>
2014-02-19 10:45:51 +0100

    Reactivate test on blog thumbnail

--- tests/casperjs/scenario/blog.js
+++ tests/casperjs/scenario/blog.js
@@ -118,8 +118,7 @@ function run() {
             this.waitForText('Blog content', function() {
                 this.test.assertSelectorHasText('#pagename', 'New blog post', 'Title OK');
                 this.test.assertTitle('New test page modified - New blog post', 'Title meta OK');
-                // @Todo : to reactivate
-                //this.test.assertResourceExists('cache/media/new-folder/logo-novius-os/', 'Logo was loaded');
+                this.test.assertResourceExists('cache/media/new-folder/logo-novius-os/', 'Logo was loaded');
                 this.test.assertSelectorHasText('.blognews_tags', 'tag-test', 'Tag OK');
                 this.test.assertSelectorHasText('.blognews_categories', 'New category', 'Category OK');
             }, function() {
